ba38b956edce85ecaf6b09f56787e3c0fb87e669
Bora <boraismyname@gmail.com>
2014-04-05 13:37:50 -0400

    Maps with multiple locations -> 4/5/2014 1:37

--- app/src/main/java/com/example/pinpointer/app/MapsActivity.java
+++ app/src/main/java/com/example/pinpointer/app/MapsActivity.java
@@ -1,15 +1,20 @@
 package com.example.pinpointer.app;
 
+import android.app.Activity;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-
+import android.view.Menu;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity {
-
+    static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+    static final LatLng KIEL = new LatLng(53.551, 9.993);
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     @Override
@@ -17,6 +22,7 @@ public class MapsActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         setUpMapIfNeeded();
+
     }
 
     @Override
@@ -51,6 +57,22 @@ public class MapsActivity extends FragmentActivity {
                 setUpMap();
             }
         }
+        mMap.setMyLocationEnabled(true);
+        Marker hamburg = mMap.addMarker(new MarkerOptions().position(HAMBURG)
+                .title("Hamburg"));
+        Marker kiel = mMap.addMarker(new MarkerOptions()
+                .position(KIEL)
+                .title("Kiel")
+                .snippet("Kiel is cool")
+                .icon(BitmapDescriptorFactory
+                        .fromResource(R.drawable.ic_launcher)));
+
+        // Move the camera instantly to hamburg with a zoom of 15.
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+
+        // Zoom in, animating the camera.
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+
     }
 
     /**
